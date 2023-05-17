5a796e5288833f1a89806286374b9d773ae3cd93
Temesgen <temesgenzewude21@gmail.com>
2023-05-17 14:42:33 +0300

    welcome module and view

--- chatty/lib/pages/frame/welcome/welcome_view.dart
+++ chatty/lib/pages/frame/welcome/welcome_view.dart
@@ -1,38 +1,42 @@
-// import 'package:chatty/common/values/colors.dart';
-// import 'package:chatty/presentation/frame/welcome/welcome_controller.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:chatty/common/values/colors.dart';
+import 'package:chatty/pages/frame/welcome/welcome_controller.dart';
 
-// class WelcomePage extends GetView<WelcomeController> {
-//   const WelcomePage({super.key});
-//   Widget _buildPageHeaderTitle(String title) {
-//     return Container(
-//       margin: EdgeInsets.only(
-//         top: 350,
-//       ),
-//       child: Text(
-//         title,
-//         textAlign: TextAlign.center,
-//         style:  TextStyle(
-//           color: AppColors.primaryElementText,
-//           fontFamily: "Montserrat",
-//           fontWeight: FontWeight.bold,
-//           fontSize: 45.sp,
-//         ),
-//       ),
-//     );
-//   }
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: AppColors.primaryElement,
-//       body: Container(
-//           width: 360.w,
-//           height: 780.h,
-//           child: _buildPageHeaderTitle(controller.title)),
-//     );
-//   }
-// }
+class WelcomePage extends GetView<WelcomeController> {
+  const WelcomePage({super.key});
+
+// for building the head title of the welcome page
+  Widget _buildPageHeaderTitle(String title) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 350,
+      ),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style:  TextStyle(
+          color: AppColors.primaryElementText,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.bold,
+          fontSize: 45.sp,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.primaryElement,
+      body: Container(
+          width: 360.w,
+          height: 780.h,
+          child: _buildPageHeaderTitle(controller.title)),
+    );
+  }
+}
+
 
