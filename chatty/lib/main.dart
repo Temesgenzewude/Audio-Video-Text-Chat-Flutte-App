546c4b283455bcac4521197187744d827baca82f
Temesgen <temesgenzewude21@gmail.com>
2023-05-18 11:10:56 +0300

    injecting dependencies globally

--- chatty/lib/main.dart
+++ chatty/lib/main.dart
@@ -1,36 +1,31 @@
-import 'package:chatty/common/routes/names.dart';
 import 'package:chatty/common/routes/pages.dart';
+import 'package:chatty/global/global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  await Global.init();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 780),
-      
-      builder: (context, child)=>GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chatty .',
-      theme: ThemeData(
-      
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    
-    ));
+        designSize: const Size(360, 780),
+        builder: (context, child) => GetMaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Chatty .',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              initialRoute: AppPages.INITIAL,
+              getPages: AppPages.routes,
+            ));
   }
 }
-
-
-
