fd998689b403fa3110b956cdd0e1cd3f0be06cfd
Temesgen <temesgenzewude21@gmail.com>
2023-05-18 09:41:09 +0300

    welcome module and main.dart

--- chatty/lib/pages/frame/welcome/welcome_view.dart
+++ chatty/lib/pages/frame/welcome/welcome_view.dart
@@ -12,6 +12,7 @@ class WelcomePage extends GetView<WelcomeController> {
   Widget _buildPageHeaderTitle(String title) {
     return Container(
       margin: EdgeInsets.only(
+
         top: 350,
       ),
       child: Text(
@@ -21,7 +22,7 @@ class WelcomePage extends GetView<WelcomeController> {
           color: AppColors.primaryElementText,
           fontFamily: "Montserrat",
           fontWeight: FontWeight.bold,
-          fontSize: 45.sp,
+          fontSize: 45,
         ),
       ),
     );
@@ -32,8 +33,8 @@ class WelcomePage extends GetView<WelcomeController> {
     return Scaffold(
       backgroundColor: AppColors.primaryElement,
       body: Container(
-          width: 360.w,
-          height: 780.h,
+          width: 360,
+          height: 780,
           child: _buildPageHeaderTitle(controller.title)),
     );
   }
