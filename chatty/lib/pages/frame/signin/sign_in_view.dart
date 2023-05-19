ddc485e4a6278d780b782f823464c39f34fbb95d
Temesgen <temesgenzewude21@gmail.com>
2023-05-19 11:45:49 +0300

    implementing signin module and third party login button

--- chatty/lib/pages/frame/signin/sign_in_view.dart
+++ chatty/lib/pages/frame/signin/sign_in_view.dart
@@ -23,53 +23,54 @@ class SignInPage extends GetView<SignInController> {
     );
   }
 
-  // Widget _buildThirdPartyLogin(String loginType, String logo) {
-  //   return GestureDetector(
-  //     onTap: () {
-  //       controller.handleSignIn("google");
-  //     },
-  //     child: Container(
-  //       width: 295.w,
-  //       height: 44.h,
-  //       padding: EdgeInsets.all(10.h),
-  //       margin: EdgeInsets.only(bottom: 15.h),
-  //       decoration: BoxDecoration(
-  //           color: AppColors.primaryBackground,
-  //           borderRadius: BorderRadius.all(
-  //             Radius.circular(5.h),
-  //           ),
-  //           boxShadow: [
-  //             BoxShadow(
-  //                 color: Colors.grey.withOpacity(0.1),
-  //                 spreadRadius: 1,
-  //                 blurRadius: 2,
-  //                 offset: const Offset(0, 1)),
-  //           ]),
-  //       child: Row(
-  //           mainAxisAlignment: logo.isEmpty
-  //               ? MainAxisAlignment.center
-  //               : MainAxisAlignment.start,
-  //           children: [
-  //             logo.isEmpty
-  //                 ? Container()
-  //                 : Container(
-  //                     padding: EdgeInsets.only(left: 40.w, right: 30.w),
-  //                     child: Image.asset("assets/icons/$logo.png"),
-  //                   ),
-  //             Container(
-  //               child: Text(
-  //                 "Sign In with $loginType",
-  //                 style: TextStyle(
-  //                   color: AppColors.primaryText,
-  //                   fontWeight: FontWeight.normal,
-  //                   fontSize: 14.sp,
-  //                 ),
-  //               ),
-  //             )
-  //           ]),
-  //     ),
-  //   );
-  // }
+  Widget _buildThirdPartyLogin(String loginType, String logo) {
+    return GestureDetector(
+      onTap: () {
+        // controller.handleSignIn("google");
+      },
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        padding: EdgeInsets.all(10.h),
+        margin: EdgeInsets.only(bottom: 15.h),
+        decoration: BoxDecoration(
+            color: AppColors.primaryBackground,
+            borderRadius: BorderRadius.all(
+              Radius.circular(5.h),
+            ),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1)),
+            ]),
+        child: Row(
+            mainAxisAlignment: logo.isEmpty
+                ? MainAxisAlignment.center
+                : MainAxisAlignment.start,
+            children: [
+              logo.isEmpty
+                  ? Container()
+                  : Container(
+                      padding: EdgeInsets.only(left: 40.w, right: 30.w),
+                      child: Image.asset("assets/icons/$logo.png"),
+                    ),
+              Container(
+                child: Text(
+                  "Sign In with $loginType",
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.sp,
+                  ),
+                ),
+              )
+            ]),
+      ),
+    );
+  }
+
 
   // Widget _buildOrWidget() {
   //   return Container(
@@ -133,7 +134,7 @@ class SignInPage extends GetView<SignInController> {
         child: Column(
           children: [
             _buildLogo(),
-            // _buildThirdPartyLogin("Google", "google"),
+            _buildThirdPartyLogin("Google", "google"),
             // _buildThirdPartyLogin("Facebook", "facebook"),
             // _buildThirdPartyLogin("Apple", "apple"),
             // _buildOrWidget(),
