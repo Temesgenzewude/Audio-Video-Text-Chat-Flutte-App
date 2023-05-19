36efa4be42a4f6b348a1ba01b213eb12d91f1f3b
Temesgen <temesgenzewude21@gmail.com>
2023-05-19 11:58:44 +0300

    implementing signin module phone number login part 1

--- chatty/lib/pages/frame/signin/sign_in_view.dart
+++ chatty/lib/pages/frame/signin/sign_in_view.dart
@@ -30,7 +30,7 @@ class SignInPage extends GetView<SignInController> {
       },
       child: Container(
         width: 295.w,
-        height: 44.h,
+        height: 50.h,
         padding: EdgeInsets.all(10.h),
         margin: EdgeInsets.only(bottom: 15.h),
         decoration: BoxDecoration(
@@ -72,30 +72,30 @@ class SignInPage extends GetView<SignInController> {
   }
 
 
-  // Widget _buildOrWidget() {
-  //   return Container(
-  //     margin: EdgeInsets.only(top: 20.h, bottom: 35.h),
-  //     child: Row(
-  //       children: [
-  //         Expanded(
-  //           child: Divider(
-  //             indent: 50,
-  //             height: 2.h,
-  //             color: AppColors.primarySecondaryElementText,
-  //           ),
-  //         ),
-  //         const Text("  or  "),
-  //         Expanded(
-  //           child: Divider(
-  //             endIndent: 50,
-  //             height: 2.h,
-  //             color: AppColors.primarySecondaryElementText,
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
+  Widget _buildOrWidget() {
+    return Container(
+      margin: EdgeInsets.only(top: 20.h, bottom: 35.h),
+      child: Row(
+        children: [
+          Expanded(
+            child: Divider(
+              indent: 50,
+              height: 2.h,
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+          const Text("  or  "),
+          Expanded(
+            child: Divider(
+              endIndent: 50,
+              height: 2.h,
+              color: AppColors.primarySecondaryElementText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   // Widget _buildSignInWidget() {
   //   return Column(
@@ -135,9 +135,9 @@ class SignInPage extends GetView<SignInController> {
           children: [
             _buildLogo(),
             _buildThirdPartyLogin("Google", "google"),
-            // _buildThirdPartyLogin("Facebook", "facebook"),
-            // _buildThirdPartyLogin("Apple", "apple"),
-            // _buildOrWidget(),
+            _buildThirdPartyLogin("Facebook", "facebook"),
+            _buildThirdPartyLogin("Apple", "apple"),
+            _buildOrWidget(),
             // _buildThirdPartyLogin("phone number", ""),
             // SizedBox(
             //   height: 30.h,
