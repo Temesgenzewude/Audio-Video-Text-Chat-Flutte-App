74afb8bb892b42a13de03664277d390af277d9c1
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-05-26 23:32:09 +0300

    Message Module and Headbar

--- chatty/lib/pages/message/message_view.dart
+++ chatty/lib/pages/message/message_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -8,9 +7,22 @@ import 'message_index.dart';
 class MessagePage extends GetView<MessageController> {
   const MessagePage({super.key});
 
+  // silvers are used inside listviews  and are built lazily and are good for performance
+  // Are generally used under CustomScrollView
+
+  Widget _headBar() {
+    return Center();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(children: [
+        CustomScrollView(
+          slivers: [],
+        )
+      ]),
+    ));
   }
 }
