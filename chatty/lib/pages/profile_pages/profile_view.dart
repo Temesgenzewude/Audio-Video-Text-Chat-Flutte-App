baf94de9e29611f00a7df898ec68cf5c9a0aff5c
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-05-29 13:38:20 +0300

    implemented profile module logout dialogue button

--- chatty/lib/pages/profile_pages/profile_view.dart
+++ chatty/lib/pages/profile_pages/profile_view.dart
@@ -5,7 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ProfilePage extends GetView<WelcomeController> {
+import 'profile_index.dart';
+
+class ProfilePage extends GetView<ProfileController> {
   const ProfilePage({super.key});
 
 // for building the head title of the profile page
@@ -79,12 +81,8 @@ class ProfilePage extends GetView<WelcomeController> {
                   spreadRadius: 1,
                   blurRadius: 2,
                   offset: const Offset(0, 1))
-     
             ]),
-            child:Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: [
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           Text(
             "Complete",
             textAlign: TextAlign.center,
@@ -95,7 +93,53 @@ class ProfilePage extends GetView<WelcomeController> {
           )
         ]),
       ),
+    );
+  }
 
+  Widget _buildLogoutBtn() {
+    return GestureDetector(
+      child: Container(
+        width: 295.w,
+        height: 44.h,
+        margin: EdgeInsets.only(top: 60.h, bottom: 30.h),
+        decoration: BoxDecoration(
+            color: AppColors.primarySecondaryElementText,
+            borderRadius: BorderRadius.circular(5.w),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.1),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 1))
+            ]),
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            "Logout",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: AppColors.primaryElementText,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.normal),
+          )
+        ]),
+      ),
+      onTap: () {
+        Get.defaultDialog(
+          title:"Are you sure you want to logout?",
+          content: Container(),
+          onConfirm: (){
+
+          } ,
+          onCancel: (){
+            
+            
+
+          },
+          textCancel: "Cancel",
+          textConfirm: "Logout",
+          confirmTextColor: Colors.white
+        );
+      },
     );
   }
 
@@ -108,7 +152,13 @@ class ProfilePage extends GetView<WelcomeController> {
             SliverToBoxAdapter(
               child: Container(
                   child: Column(
-                children: [_buildProfilePhoto(), _buildCompleteBtn()],
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _buildProfilePhoto(),
+                  _buildCompleteBtn(),
+                  _buildLogoutBtn()
+                ],
               )),
             )
           ]),
