709d859e38e7743abc4a7cf518d47116f71d5dd0
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-05-29 10:13:38 +0300

    added Message module profile icon

--- chatty/lib/pages/message/message_state.dart
+++ chatty/lib/pages/message/message_state.dart
@@ -1,5 +1,6 @@
+import 'package:chatty/common/entities/entities.dart';
+import 'package:get/get.dart';
 
-class MessageState{
-
-  
+class MessageState {
+  var head_detail = UserItem().obs;
 }
--- chatty/lib/pages/message/message_view.dart
+++ chatty/lib/pages/message/message_view.dart
@@ -1,3 +1,4 @@
+import 'package:chatty/common/values/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,7 +12,46 @@ class MessagePage extends GetView<MessageController> {
   // Are generally used under CustomScrollView
 
   Widget _headBar() {
-    return Center();
+    return Center(
+        child: Container(
+            width: 320.w,
+            height: 44.w,
+            margin: EdgeInsets.only(
+              bottom: 20.h,
+              top: 20.h,
+            ),
+            child: Row(
+              children: [
+                Stack(
+                  children: [
+                    GestureDetector(
+                      child: Container(
+                        width: 44.h,
+                        height: 44.h,
+                        decoration: BoxDecoration(
+                            color: AppColors.primarySecondaryBackground,
+                            borderRadius: BorderRadius.all(
+                              (Radius.circular(22.h)),
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.red.withOpacity(0.1),
+                                  spreadRadius: 1,
+                                  blurRadius: 2,
+                                  offset: const Offset(0, 1))
+                            ]),
+                        child: controller.state.head_detail.value.avatar == null
+                            ? Image(
+                                image: AssetImage(
+                                    "assets/images/account_header.png"),
+                              )
+                            : Text("Hi"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
 
   @override
@@ -20,7 +60,12 @@ class MessagePage extends GetView<MessageController> {
         body: SafeArea(
       child: Stack(children: [
         CustomScrollView(
-          slivers: [],
+          slivers: [
+            SliverAppBar(
+              title: _headBar(),
+              pinned: true,
+            )
+          ],
         )
       ]),
     ));
