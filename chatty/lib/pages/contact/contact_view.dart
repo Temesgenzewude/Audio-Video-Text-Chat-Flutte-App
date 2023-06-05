e26fe217d0f1cd17014baa544f3052733cbb40df
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-06-05 23:58:54 +0300

    added app bar for Contact Module

--- chatty/lib/pages/contact/contact_view.dart
+++ chatty/lib/pages/contact/contact_view.dart
@@ -7,29 +7,27 @@ import 'package:chatty/common/values/values.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ContactPage extends GetView<ContactController> {
-  // AppBar _buildAppBar() {
-  //   return AppBar(
-  //     // backgroundColor: Colors.transparent,
-  //     // elevation: 0,
-  //       title: Text(
-  //         "Contact",
-  //         style: TextStyle(
-  //           color: AppColors.primaryText,
-  //           fontSize: 16.sp,
-  //           fontWeight: FontWeight.normal,
-  //         ),
-  //       ));
-  // }
+  AppBar _buildAppBar() {
+    return AppBar(
+      // backgroundColor: Colors.transparent,
+      // elevation: 0,
+        title: Text(
+          "Contact",
+          style: TextStyle(
+            color: AppColors.primaryText,
+            fontSize: 16.sp,
+            fontWeight: FontWeight.normal,
+          ),
+        ));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(child: Text("Contact Page")),
-      ),
+     
 
-      // appBar: _buildAppBar(),
-      // body: ContactList(),
+      appBar: _buildAppBar(),
+      body: ContactList(),
     );
   }
 }
