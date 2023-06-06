dc5971d9104c30f45eb71b51aa9bee3afbafd786
Temesgen Zewude <temesgenzewude21@gmail.com>
2023-06-06 09:21:45 +0300

    testing contact module API on Postman

--- chatty/lib/common/store/user.dart
+++ chatty/lib/common/store/user.dart
@@ -19,13 +19,14 @@ class UserStore extends GetxController {
   UserItem get profile => _profile.value;
   bool get hasToken => token.isNotEmpty;
   // set setIsLogin(login) => _isLogin.value = login;
-  
 
   @override
   void onInit() {
     super.onInit();
     token = StorageService.to.getString(STORAGE_USER_TOKEN_KEY);
     var profileOffline = StorageService.to.getString(STORAGE_USER_PROFILE_KEY);
+
+    print(profileOffline);
     if (profileOffline.isNotEmpty) {
       _isLogin.value = true;
       _profile(UserItem.fromJson(jsonDecode(profileOffline)));
